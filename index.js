766e979bcb640c8a3c62c14de6d7855b5e12ea02
khadetou <khadetou96@gmail.com>
2021-12-05 00:07:19 +0000

    starting a server

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world!")
+/**
+ *
+ * Primary file for the API
+ *
+ */
+// Dependencies
+const http = require("http");
+
+//the server should respond to all requests with a string
+const server = http.createServer((req, res) => {
+  res.end("It works");
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, () => {
+  console.log("The server is listening on port 3000");
+});
