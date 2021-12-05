045279ed043fc3fade972efaa46461af66224892
khadetou <khadetou96@gmail.com>
2021-12-05 00:30:51 +0000

    parsing http requests

--- index.js
+++ index.js
@@ -5,10 +5,21 @@
  */
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 //the server should respond to all requests with a string
 const server = http.createServer((req, res) => {
+  //Get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  //Get the path
+  const path = parsedUrl.pathname;
+  const trimedPath = path.replace(/^\/+|\/+$/g, "");
+
+  //Send the response
   res.end("It works");
+
+  // log the request path
+  console.log(`Request for ${trimedPath} received`);
 });
 
 // Start the server and have it listen on port 3000
